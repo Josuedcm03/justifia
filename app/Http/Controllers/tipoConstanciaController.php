ae44b73e85424468395ab7e678c359104f3d9437
Arnia <arni.arroliga8@gmail.com>
2025-06-07 23:45:36 -0600

    Creación de controladores con funciones del CRUD vacías y asociadas a su respectivo modelo

---
+++ app/Http/Controllers/tipoConstanciaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tipoConstancia;
+use Illuminate\Http\Request;
+
+class tipoConstanciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(tipoConstancia $tipoConstancia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(tipoConstancia $tipoConstancia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, tipoConstancia $tipoConstancia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(tipoConstancia $tipoConstancia)
+    {
+        //
+    }
+}
